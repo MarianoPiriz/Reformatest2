c58322cbaa83d2851e0500658fb3032295467061
Mariano Piriz <65415907+MarianoPiriz@users.noreply.github.com>
2020-10-01 21:56:45 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,13 +1,13 @@
 <?php
 #$host = "";
 #$user ="id14031906_reformausrdb";
-#$pass ="|m%7nVYA4(a]$)/A";
+#$pass ="**********";
 #$db = "id14031906_reformadb";
 
 #$conexion = mysqli_connect($host,$user,$pass,$db);
 
 
-$conexion = new mysqli("localhost", "id14031906_reformausrdb", "7d8b36e406b429906cdffe16afc614435f26c470cf678a6f7a23d3294c240a93", "id14031906_reformadb");
+$conexion = new mysqli("localhost", "id14031906_reformausrdb", "**************", "id14031906_reformadb");
 
 
 
